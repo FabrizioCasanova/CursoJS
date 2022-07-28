0c0ac064dba1474dfdeb668e81fa410a686ccee8
Fabrizio Julian Casanova <fabri.casanova@outlook.com>
2022-07-27 21:04:38 -0300

    Creacion de simulador interactivo con funciones

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-// Calculador de impuesto al valor agregado (IVA)
-let numero
+const calculoPromedio = (nota1,nota2,nota3) => nota1 + nota2 + nota3 
+
+let primerNota,segundaNota,tercerNota
 let pregunta
+
 do {
-    numero = parseFloat(prompt('Ingrese el numero a calcular'))
-        if (numero) {alert (`El IVA de este numero es: ${numero * 21 /100}`)
-            pregunta = prompt('¿Desea ingresar otro numero a calcular?').toLowerCase()}
+    primerNota = parseFloat(prompt('Ingrese la nota que tiene el/la alumno/a en su primer trimestre'))
+    segundaNota = parseFloat(prompt('Ingrese la nota que tiene el/la alumno/a en su segundo trimestre'))
+    tercerNota = parseFloat(prompt('Ingrese la nota que tiene el/la alumno/a en su tercer trimestre'))
 
-    if (isNaN(numero)) { 
-        alert('Por favor, ingrese un numero valido')}
+    if (primerNota,segundaNota,tercerNota){
+    alert (`El promedio de este/a alumno/a es: ${calculoPromedio(primerNota,segundaNota,tercerNota*3/3)}`)
+    pregunta = prompt('¿Desea calcular otro promedio? En ese caso, por favor, escriba "Si"').toLowerCase()}    
 
-} while (isNaN(numero) || pregunta =='si')
+    if (isNaN(primerNota) || isNaN(segundaNota) || isNaN(tercerNota)) {
+        alert ('Numero no valido. Por favor, ingrese un numero valido')
+    }
+
+} while ((isNaN(primerNota)) || (isNaN(segundaNota)) || (isNaN(tercerNota)) || pregunta == 'si' )
+
+
 
